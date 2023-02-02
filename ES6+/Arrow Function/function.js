d593f36981ba4bdfcd42984f17e7834853ec5409
ergnalbayrak <ergnalbayrak@gmail.com>
2023-02-02 21:44:23 +0300

    ES6+ lessons done

---
+++ ES6+/Arrow Function/function.js
@@ -0,0 +1,28 @@
+// const merhaba = function (){
+//     console.log("Merhaba");
+// }
+
+// merhaba();
+
+// Arrow Function
+
+// const merhaba = () => {
+//     console.log("Merhaba");
+// }
+//     merhaba();
+
+// const merhaba = firstName => {
+//  console.log("Merhaba",firstName);
+
+// }
+
+// merhaba("Ergun");
+
+// const cube = function (x){
+//     return x * x* x;
+// }
+// console.log(cube(4));
+
+const cube = x =>  x* x* x;
+
+console.log(cube(4));
---
+++ ES6+/Arrow Function/references.js
@@ -0,0 +1,39 @@
+//  let a = "Ergun";
+//  let b = "Ergun";
+
+//  if (a === b){
+//     console.log("Eşit");
+//  }
+
+
+// let array1 = [1, 2, 3, 4,5];
+// let array2 = [1, 2, 3, 4,5];
+
+// if ( array1 === array2 ) {
+//     console.log("Eşit");
+
+
+// }
+
+// const person1 = {
+//     name: "Ergun",
+//     age: 26
+// }
+
+// const person2 = {
+//     name: "Ergun",
+//     age: 26
+// }
+// if (person1 === person2) {
+//     console.log("Eşit");
+
+// }
+
+// const cities = new Map();
+// const key = [1,2,3]
+// cities.set("Ankara",5);
+// cities.set("İstanbul",15);
+// cities.set(key,"Array");
+
+// console.log(cities.get("Ankara"));
+// console.log(cities.get(key));
