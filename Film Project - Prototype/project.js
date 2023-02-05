1af85bce423bd30c0f0818d8ff19336a02ca663c
ergnalbayrak <ergnalbayrak@gmail.com>
2023-02-05 23:05:39 +0300

    film project

---
+++ Film Project - Prototype/project.js
@@ -0,0 +1,43 @@
+const form = document.getElementById("film-form");
+const titleElement = document.querySelector("#title");
+const directorElement = document.querySelector("#director");
+const urlElement = document.querySelector("#url");
+
+// UI Objesini Başlatma
+const ui = new UI();
+
+// Tüm eventleri yükleme 
+
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener("submit", addFilm);
+    document.addEventListener("DOMContentLoaded", function () {
+        let films = storage.getFilmsFromStorage();
+        ui.loadAllFilms(films);
+    });
+}
+function addFilm(e) {
+    const title = titleElement.value;
+    const director = directorElement.value;
+    const url = urlElement.value;
+
+    if (title === "" || director === "" || url === "") {
+        // Hata    
+        ui.displayMessages("Tüm alanları doldurun...", "danger");
+
+    }
+    else {
+        // Yeni Film
+        const newFilm = new Film(title, director, url);
+
+        ui.addFilmToUI(newFilm); // Arayüze film ekleme
+        ui.displayMessages("Film başarıyla eklendi...", "success");
+
+
+    }
+
+    ui.clearInputs(titleElement, urlElement, directorElement);
+
+    e.preventDefault();
+}
